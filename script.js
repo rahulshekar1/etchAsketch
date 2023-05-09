179c813274157a4bc3b888cf55a2171fcde5ee9b
rahulshekar1 <rahulshekar71@gmail.com>
2023-05-09 18:00:02 +0530

    partial completes creating grid according to user input

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-for(let i=0; i<(16*16); i++){
-    let boxes = document.createElement('div');
-    boxes.classList.add("tiles");
-    document.getElementById('row1').appendChild(boxes);
+createGrid(16);
+function createGrid(gridSize){
+    for(let i=0; i<((gridSize)**2); i++){
+        let boxes = document.createElement('div');
+        boxes.classList.add("tiles");
+        document.getElementById('row1').appendChild(boxes);
+    }
+    colorGrid();
 }
-
-const selectTiles = document.querySelectorAll(".tiles");
-selectTiles.forEach(item=>{
+let sub = document.querySelector('.sub')
+sub.addEventListener('click',()=>{
+    a = document.querySelectorAll(".tiles")
+    a.forEach(d=>{d.remove();})
+    let gridSize = document.getElementById('Uinp').value;
+    createGrid(gridSize);
+})
+function colorGrid(){
+    const selectTiles = document.querySelectorAll(".tiles");
+    selectTiles.forEach(item=>{
     item.addEventListener('mouseover',() => {
         item.style.backgroundColor ="blue";
         })
     })
-const btn = document.querySelector("#btn");
+}
 
+const btn = document.querySelector("#btn");
 function clearGrid(){
     selectTiles.forEach(item=>{item.style.backgroundColor="white"});
     }
 btn.addEventListener('click',clearGrid);
-
 
 
