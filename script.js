d78ed56ee4687518b2d8c518e73efa2bca5e88c7
rahulshekar1 <rahulshekar71@gmail.com>
2023-05-11 18:14:45 +0530

    resize the tiles dynamically within container restructure the code

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 createGrid(16);
-function createGrid(gridSize){
-    for(let i=0; i<((gridSize)**2); i++){
+function createGrid(gridInp){
+    for(let i=0; i<((gridInp)**2); i++){
         let boxes = document.createElement('div');
         boxes.classList.add("tiles");
         document.getElementById('row1').appendChild(boxes);
     }
     colorGrid();
 }
-let sub = document.querySelector('.sub')
-sub.addEventListener('click',()=>{
-    a = document.querySelectorAll(".tiles")
-    a.forEach(d=>{d.remove();})
-    let gridSize = document.getElementById('Uinp').value;
-    createGrid(gridSize);
-})
+
 function colorGrid(){
-    const selectTiles = document.querySelectorAll(".tiles");
-    selectTiles.forEach(item=>{
+    document.querySelectorAll(".tiles").forEach(item=>{
     item.addEventListener('mouseover',() => {
         item.style.backgroundColor ="blue";
         })
     })
 }
 
-const btn = document.querySelector("#btn");
+
+function gridResize(){
+    document.querySelectorAll(".tiles").forEach(tile=>{tile.remove();})
+    let gridInp = document.getElementById('Uinp').value;
+    console.log(gridInp);
+    let gridSize = (500/gridInp);
+    console.log(gridSize);
+    createGrid(gridInp);
+    document.querySelectorAll(".tiles").forEach(item =>{
+        item.style["height"] = gridSize  + "px";
+        item.style["width"] = gridSize  + "px";
+        }
+    );
+}
+
+let submitButton = document.querySelector('.submitButton')
+submitButton.addEventListener('click',gridResize);
+
+
 function clearGrid(){
-    selectTiles.forEach(item=>{item.style.backgroundColor="white"});
+    document.querySelectorAll(".tiles").forEach(item=>{item.style.backgroundColor="white"});
     }
-btn.addEventListener('click',clearGrid);
 
-
+const reset = document.querySelector("#reset");
+reset.addEventListener('click',clearGrid);
